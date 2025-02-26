d168913aeabeef38215ab2224e8334456ffb17a4
Zeeshan Ahmad Bhatti <156796090+zeeshanabhatti@users.noreply.github.com>
2025-02-26 15:25:42 +0500

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 // Array of users with page-specific credentials
 const users = [
-    { username: 'monica', password: '2025', expiry: new Date('2025-12-31'), pageTitle: 'book2Manners || Bonus' },
+    { username: 'monica', password: '2025', expiry: new Date('2025-12-31'), pageTitle: 'Bonus' },
     { username: 'user1', password: 'pass1', expiry: new Date('2024-12-31'), pageTitle: 'Overlay Login Box' },
     { username: 'user2', password: 'pass2', expiry: new Date('2024-10-01'), pageTitle: 'Overlay Login Box' },
     { username: 'user3', password: 'pass3', expiry: new Date('2023-01-01'), pageTitle: 'Another Page Title' }
